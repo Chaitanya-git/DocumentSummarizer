b363b3945e9a4d8820f27097846514bbf58f7527
Chaitanya <chaitanya2.1999@gmail.com>
2020-08-09 01:12:50 +0530

    Configure setuptools

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Chaitanya-git", # Replace with your own username
+    version="0.0.1",
+    author="Chaitanya",
+    author_email="",
+    description="A simple document summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chaitanya-git/DocumentSummarizer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "fire",
+        "bs4",
+        "pprint",
+        "nltk",
+        "sklearn",
+        "numpy"
+    ],
+)
